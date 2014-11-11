cbf0efd1886725dd39946ffb09fb98c8fd358239
Dan Midwood <dan@danmidwood.com>
2014-11-11 11:54:50 -0300

    Add a nondeterministic finite state machine
    In this change we take our work on the DFA and build on it to create a
    non-deterministic variant, aka a NFA. Where the DFA has only a single
    current state the NFA can hold multiple in parallel.

--- pt1_03_the_simplest_computers.rb
+++ pt1_03_the_simplest_computers.rb
@@ -182,3 +182,151 @@ end
 # our machine to be in more than one state at once. But I do think it will need
 # to "collapse" into a single state for acceptance, and that should be determin-
 # -istic based on the input. We will see..
+
+# The book talks about two possible ways to simulate a NFA, one by traversing
+# depth first and a second by traversing breadth first.
+# The breadth first approach is interesting, by spawning additional threads at
+# each branch point there can be multiple machines reading in paralel.
+# In the depth first approach, it sounds like we'll be repeating a lot of the
+# processing.
+# The machine will be snapshotted at the first branch point, all input from then
+# onwards will be retained and, on new input, everything has to be run from the
+# branch point, traversing up and down all of the branches until an accept state
+# is hit or the traversal is exhauset. On exhausation, the machine will wait for
+# the next input character and try the whole thing all over again.
+
+# Nevermind though, the author deems both of these implementations complicated
+# and inefficient (I agree), so we're going to build something much simpler.
+# And, fortunately, there is an easy way.
+
+# Our strategy is going to be to record the current state as a set of possible
+# states, where applying rules for some input maps the set of possible states to
+# another set of possible states.
+# When we're in a state it doesn't matter how we arrived there, and with this
+# strategy there's no simple way to know what it was. To explain further, if our
+# input so far gives us more than one path to land in a specific state then that
+# state will be a member of our possible states set. The state will only be
+# recorded in there once (because it is a set) and it will be without the
+# histories to track it back.
+
+# Btw, not that the machine should track things back. To an inputter it's just a
+# black box spitting out [accept|not accept] states.
+
+# So, let's add a new Rulebook to handle multiple current states.
+require 'set'
+
+class NFARulebook < Struct.new(:rules)
+  def next_states(states, character)
+    states.flat_map { |state|
+      follow_rules_for(state, character)
+    }.to_set
+  end
+
+  def follow_rules_for(state, character)
+    rules_for(state, character).map(&:follow)
+  end
+
+  def rules_for(state, character)
+    rules.select { |rule| rule.applies_to?(state, character) }
+  end
+end
+
+class NFARulebookTest < Test::Unit::TestCase
+  def test_rulebook
+    rulebook = NFARulebook.new([FARule.new(1, 'a', 1), FARule.new(1, 'b', 1),
+                                FARule.new(1, 'b', 2), FARule.new(2, 'a', 3),
+                                FARule.new(2, 'b', 3), FARule.new(3, 'a', 4),
+                                FARule.new(3, 'b', 4)]);
+
+    assert_equal(Set[1, 2], rulebook.next_states(Set[1], 'b'))
+    assert_equal(Set[1, 3], rulebook.next_states(Set[1, 2], 'a'))
+    assert_equal(Set[1, 2, 4], rulebook.next_states(Set[1, 3], 'b'))
+
+  end
+end
+
+# Now we can create a machine
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
+  def accepting?
+    # This is really nice, the & character performs a set intersection and
+    # `any?` tells us whether the result contains anything, i.e. a current state
+    # that is also an accept state
+    (current_states & accept_states).any?
+  end
+end
+
+class NFATest < Test::Unit::TestCase
+  def test_nfa
+    rulebook = NFARulebook.new([FARule.new(1, 'a', 1), FARule.new(1, 'b', 1),
+                                FARule.new(1, 'b', 2), FARule.new(2, 'a', 3),
+                                FARule.new(2, 'b', 3), FARule.new(3, 'a', 4),
+                                FARule.new(3, 'b', 4)]);
+
+    assert_equal(false, NFA.new(Set[1], [4], rulebook).accepting?)
+    assert_equal(true, NFA.new(Set[1, 2, 4], [4], rulebook).accepting?)
+  end
+end
+
+
+class NFA
+  def read_character(character)
+    self.current_states = rulebook.next_states(current_states, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+end
+
+class NFACharStringTest < Test::Unit::TestCase
+  def test_nfa
+    rulebook = NFARulebook.new([FARule.new(1, 'a', 1), FARule.new(1, 'b', 1),
+                                FARule.new(1, 'b', 2), FARule.new(2, 'a', 3),
+                                FARule.new(2, 'b', 3), FARule.new(3, 'a', 4),
+                                FARule.new(3, 'b', 4)]);
+
+    nfa = NFA.new(Set[1], [4], rulebook);
+    assert_equal(false, nfa.accepting?)
+    nfa.read_character('b')
+    assert_equal(false, nfa.accepting?)
+    nfa.read_character('a')
+    assert_equal(false, nfa.accepting?)
+    nfa.read_character('b')
+    assert_equal(true, nfa.accepting?)
+
+    # And again, with Strings
+    nfa = NFA.new(Set[1], [4], rulebook);
+    assert_equal(false, nfa.accepting?)
+    nfa.read_string('bbbbb')
+    assert_equal(true, nfa.accepting?)
+
+
+  end
+end
+
+# And now let's wrap the machine creation into a "design" that allows us to test
+# strings on fresh instances of the machine
+class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
+  def accepts?(string)
+    to_nfa.tap{ |nfa| nfa.read_string(string) }.accepting?
+  end
+
+  def to_nfa
+    NFA.new(Set[start_state], accept_states, rulebook)
+  end
+end
+
+class NFADesignTest < Test::Unit::TestCase
+  def test_accepts
+    rulebook = NFARulebook.new([FARule.new(1, 'a', 1), FARule.new(1, 'b', 1),
+                                FARule.new(1, 'b', 2), FARule.new(2, 'a', 3),
+                                FARule.new(2, 'b', 3), FARule.new(3, 'a', 4),
+                                FARule.new(3, 'b', 4)]);
+    design = NFADesign.new(1, [4], rulebook)
+    assert_equal(true, design.accepts?('bab'))
+    assert_equal(true, design.accepts?('bbbbb'))
+    assert_equal(false, design.accepts?('bbabb'))
+  end
+end
